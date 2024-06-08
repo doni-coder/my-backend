40a596514b4801297ed5418919cf77e258c33866
om prakash sahu <114207732+doni-coder@users.noreply.github.com>
2024-06-08 19:02:20 +0530

    phase 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
-import express from "express"
+import express from "express";
 import connectDB from "./db/index.js";
-import dotenv from "dotenv"
-dotenv.config({path:"./env"})
+import dotenv from "dotenv";
+dotenv.config({ path: "./env" });
+import { app } from "./app.js";
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("DB connection failed", error);
+  });
 
 /* -----------
 const app = express()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+
+
+const asyncHandler = (requestHandler)=>{
+    (req,res,next) =>{
+        Promise.resolve(
+            requestHandler(req,res,next)
+        ).catch((err)=> next(err))
+    }
+}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+export { asyncHandler };
+
+
+// const handleAddition = (func)=>(a,b)=>{
+//     return func(a,b)
+// }
+
+// const add=(x,y)=>{
+//     return x+y
+// }
+// const result = handleAddition(add)
+
+// console.log(result(5,5));
